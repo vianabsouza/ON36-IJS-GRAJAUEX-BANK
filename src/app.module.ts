edbf54651db7ef9bc65b6afe1e835a940fedbd6d
Barbara Viana <vianabsouza@gmail.com>
2024-07-26 19:50:07 -0300

    fix: imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,10 @@ import { GerenteModule } from './gerente/gerente.module';
 import { ContaModule } from './conta/conta.module';
 import { ClienteModule } from './cliente/cliente.module';
 import { ClienteController } from './cliente/cliente.controller';
-import { ClienteController } from './cliente/cliente.controller';
-import { ClienteModule } from './cliente/cliente.module';
 import { ContaService } from './conta/conta.service';
 import { ContaController } from './conta/conta.controller';
-import { ContaModule } from './conta/conta.module';
 import { GerenteService } from './gerente/gerente.service';
 import { GerenteController } from './gerente/gerente.controller';
-import { GerenteModule } from './gerente/gerente.module';
-import { ClienteService } from './cliente/cliente.service';
 
 @Module({
   imports: [ClienteModule, ContaModule, GerenteModule],
