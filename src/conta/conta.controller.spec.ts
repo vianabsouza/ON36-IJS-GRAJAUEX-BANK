bc41e01f7ea7f39c526b237c947f14e3213e3628
Barbara Viana <vianabsouza@gmail.com>
2024-08-09 19:58:42 -0300

    fix: add missing providers to test files

--- src/conta/conta.controller.spec.ts
+++ src/conta/conta.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContaController } from './conta.controller';
+import { ContaService } from './conta.service';
+import { ContaFactory } from './factories/conta.factory';
+
 
 describe('ContaController', () => {
   let controller: ContaController;
@@ -7,10 +10,11 @@ describe('ContaController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ContaController],
+      providers: [ContaService, ContaFactory],
     }).compile();
 
     controller = module.get<ContaController>(ContaController);
-  });
+  })
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
--- src/conta/conta.service.spec.ts
+++ src/conta/conta.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContaService } from './conta.service';
+import { ContaFactory } from './factories/conta.factory';
 
 describe('ContaService', () => {
   let service: ContaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContaService],
+      providers: [ContaService, ContaFactory],
     }).compile();
 
     service = module.get<ContaService>(ContaService);
