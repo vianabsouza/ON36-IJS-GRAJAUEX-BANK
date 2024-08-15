fd68d8e56acb997f267b58bdaf91bc002480aa14
Barbara Viana <vianabsouza@gmail.com>
2024-08-14 23:10:22 -0300

    feat: add balance, deposit, withdraw and tax rate methods to account types

--- src/conta/conta.corrente.ts
+++ src/conta/conta.corrente.ts
@@ -1,9 +1,33 @@
 import { Cliente } from "src/cliente/cliente.model";
 import { TipoConta } from "../enums/tipo.conta";
 import { Conta } from "./conta.model";
+import { NotFoundException } from "@nestjs/common";
 
 export class ContaCorrente implements Conta {
   tipo = TipoConta.CORRENTE;
+  limiteChequeEspecial: number;
+  saldo: number;
 
-  constructor(public id: string, public cliente: Cliente, public saldo: number, limiteChequeEspecial: number) {}
+  constructor(public id: string, public cliente: Cliente, public saldoInicial: number, limiteChequeEspecial: number) {
+    this.saldo = saldoInicial;
+  }
+
+  getSaldo(): number {
+    return this.saldo;
+  }
+
+  depositar(valor: number): number {
+    if (valor === 0) {
+      throw new NotFoundException('O valor do deposito deve ser positivo')
+    }
+    this.saldo += valor;
+    return this.saldo;
+  }
+
+  sacar(valor: number): number {
+    if(valor >= this.saldo + this.limiteChequeEspecial) {
+      throw new NotFoundException('Saldo insuficiente')
+    }
+    return this.saldo -= valor;
+  }
 }
--- src/conta/conta.model.ts
+++ src/conta/conta.model.ts
@@ -1,8 +1,11 @@
 import { Cliente } from "src/cliente/cliente.model";
 import { TipoConta } from "src/enums/tipo.conta";
 export interface Conta {
-    id: string,
-    cliente: Cliente,
-    saldo: number,
-    tipo: TipoConta
+    id: string;
+    cliente: Cliente;
+    saldo: number;
+    tipo: TipoConta;
+    getSaldo(): number;
+    depositar(valor: number): number;
+    sacar(valor: number): number;
 }
